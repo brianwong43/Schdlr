4d9240cd30388b2df3cbec24b6f6d4d6cf2b6831
jtsuch <37488788+jtsuch@users.noreply.github.com>
2021-03-29 14:59:15 -0700

    Got friends list displayed
    yup

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,7 +1,26 @@
 firebase.auth().onAuthStateChanged((user) => {
     if(user) {
         console.log("User logged in Profile");
-        printDisplayName();
+        // Print Display Name
+        document.getElementById("profileName").innerHTML = user.displayName;
+
+        // Get friends list
+        db.collection('users').doc(user.uid).get().then((doc) => {
+          if(doc.exists){
+            console.log("Document data:", doc.data());
+            var userObject = doc.data();
+            var friendList = userObject.friends;
+
+            // Loop through list to add friend list element
+            for(var i=0; i<friendList.length; i++){
+              var friend = document.createElement("LI");
+              friend.innerHTML = friendList[i];
+              document.getElementById("profileFriends").appendChild(friend);
+            }
+          } else {
+            console.log("No such document");
+          }
+        });
     } else {
       console.log("User logged out Profile");
     }
