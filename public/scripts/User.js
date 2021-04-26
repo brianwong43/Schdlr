8d000a96da6fcad5025993d79b4ad7085ab0b657
jtsuch <37488788+jtsuch@users.noreply.github.com>
2021-04-26 13:22:44 -0700

    Calendar stuff
    yep

--- public/scripts/User.js
+++ public/scripts/User.js
@@ -1,12 +1,13 @@
 class User {
-    constructor(uid, displayName, friends, photoURL) {
+    constructor(uid, displayName, friends, photoURL, calendarID) {
         this.uid = uid;
         this.displayName = displayName;
         this.friends = friends;
         this.photoURL = photoURL;
+        this.calendarID = calendarID;
     }
     toString() {
-        return this.uid + ', ' + this.displayName + ', ' + this.friends + ', ' + this.photoURL;
+        return this.uid + ', ' + this.displayName + ', ' + this.friends + ', ' + this.photoURL + ', ' + this.calendarID;
     }
 }
 
@@ -16,11 +17,12 @@ var userConverter = {
             uid: user.uid,
             displayName: user.displayName,
             friends: user.friends,
-            photoURL: user.photoURL
+            photoURL: user.photoURL,
+            calendarID: user.calendarID
             };
     },
     fromFirestore: function(snapshot, options){
         const data = snapshot.data(options);
-        return new User(data.uid, data.displayName, data.friends, data.photoURL);
+        return new User(data.uid, data.displayName, data.friends, data.photoURL, data.calendarID);
     }
 };
